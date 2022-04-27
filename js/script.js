f6f02753b3021ae8c87206755486a832a0e9f18f
Lucas Braga <lucasbraga_a@outlook.com>
2022-04-27 19:22:37 -0300

    funcionalidade-com-js

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// tab por navegação ao clicar no animal aparece a informação dele ao loado
+function initTabNav() {
+  const tabMenu = document.querySelectorAll(".js-tabmenu li");
+  const tabContent = document.querySelectorAll(".js-tabcontent section");
+  tabContent[0].classList.add("ativo");
+
+  if (tabMenu.length && tabContent.length) {
+    function activeTab(index) {
+      // lop  com forech para remover os ativos das outras sections
+      tabContent.forEach((section) => {
+        section.classList.remove("ativo");
+      });
+      tabContent[index].classList.add("ativo");
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener("click", () => {
+        activeTab(index);
+      });
+    });
+  }
+}
+initTabNav();
+// function que ativa e desativa os DD e DT
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-accordion dt");
+  const activeClass = "ativo";
+  if (accordionList.length) {
+    accordionList[0].classList.add(activeClass);
+    accordionList[0].nextElementSibling.classList.add(activeClass);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClass);
+      this.nextElementSibling.classList.toggle(activeClass);
+    }
+
+    accordionList.forEach((item) => {
+      item.addEventListener("click", activeAccordion);
+    });
+  }
+}
+initAccordion();
+
+// Ao clicar nos links internos ir com os scroll suave para a area
+function initScrollsuave() {
+  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
+
+  function scrollToSection(event) {
+    event.preventDefault();
+    const href = event.currentTarget.getAttribute("href");
+    const section = document.querySelector(href);
+
+    // Ativar o scrool para que ele vai suave com esse novo methodo
+    section.scrollIntoView({
+      behavior: "smooth",
+      black: "start",
+    });
+  }
+
+  linksInternos.forEach((link) => {
+    link.addEventListener("click", scrollToSection);
+  });
+}
+initScrollsuave();
