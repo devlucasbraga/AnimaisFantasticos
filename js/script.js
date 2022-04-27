25c421f44ae0ee5b800960a50d4a322c20e3bd60
Lucas Braga <lucasbraga_a@outlook.com>
2022-04-27 19:58:15 -0300

    animaçoes-no-scroll

--- js/script.js
+++ js/script.js
@@ -62,3 +62,23 @@ function initScrollsuave() {
   });
 }
 initScrollsuave();
+
+// animação no scroll quando for rolando para baixa , fazendo animação suave
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top;
+        const isSectionVisibile = sectionTop - windowMetade < 0;
+        if (isSectionVisibile) {
+          section.classList.add("ativo");
+        }
+      });
+    }
+    animaScroll();
+    window.addEventListener("scroll", animaScroll);
+  }
+}
+initAnimacaoScroll();
