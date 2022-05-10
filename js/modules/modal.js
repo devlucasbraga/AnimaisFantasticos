5bb7b681548b48cff87e8eb2291598615429d5c1
Lucas Braga <lucasbraga_a@outlook.com>
2022-05-09 22:06:55 -0300

    eventos e modal com js

---
+++ js/modules/modal.js
@@ -0,0 +1,23 @@
+export default function initModal() {
+  const botaoAbrir = document.querySelector("[data-modal=abrir]");
+  const botaoFechar = document.querySelector("[data-modal=fechar]");
+  const containerModal = document.querySelector("[data-modal=container]");
+
+  if (botaoAbrir && botaoFechar && containerModal) {
+    function toggleModal(event) {
+      event.preventDefault(); // previnir que meu evento nao saia da pagina
+      containerModal.classList.toggle("ativo");
+    }
+
+    function cliqueForaModal(event) {
+       //verifica se o clique foi fora do modal e fecha se nao ele permanece
+      if (event.target === this) {
+        toggleModal(event);
+      }
+    }
+
+    botaoAbrir.addEventListener("click", toggleModal);
+    botaoFechar.addEventListener("click", toggleModal);
+    containerModal.addEventListener("click", cliqueForaModal);
+  }
+}
--- js/script.js
+++ js/script.js
@@ -1,84 +1,11 @@
-// tab por navegação ao clicar no animal aparece a informação dele ao loado
-function initTabNav() {
-  const tabMenu = document.querySelectorAll(".js-tabmenu li");
-  const tabContent = document.querySelectorAll(".js-tabcontent section");
-  tabContent[0].classList.add("ativo");
+import initScrollSuave from "./modules/scroll-suave.js";
+import initAnimacaoScroll from "./modules/scroll-animcao.js";
+import initAccordion from "./modules/accordion.js";
+import initTabNav from "./modules/tabnav.js";
+import initModal from "./modules/modal.js";
 
-  if (tabMenu.length && tabContent.length) {
-    function activeTab(index) {
-      // lop  com forech para remover os ativos das outras sections
-      tabContent.forEach((section) => {
-        section.classList.remove("ativo");
-      });
-      tabContent[index].classList.add("ativo");
-    }
-
-    tabMenu.forEach((itemMenu, index) => {
-      itemMenu.addEventListener("click", () => {
-        activeTab(index);
-      });
-    });
-  }
-}
-initTabNav();
-// function que ativa e desativa os DD e DT
-function initAccordion() {
-  const accordionList = document.querySelectorAll(".js-accordion dt");
-  const activeClass = "ativo";
-  if (accordionList.length) {
-    accordionList[0].classList.add(activeClass);
-    accordionList[0].nextElementSibling.classList.add(activeClass);
-
-    function activeAccordion() {
-      this.classList.toggle(activeClass);
-      this.nextElementSibling.classList.toggle(activeClass);
-    }
-
-    accordionList.forEach((item) => {
-      item.addEventListener("click", activeAccordion);
-    });
-  }
-}
-initAccordion();
-
-// Ao clicar nos links internos ir com os scroll suave para a area
-function initScrollsuave() {
-  const linksInternos = document.querySelectorAll(".js-menu a[href^='#']");
-
-  function scrollToSection(event) {
-    event.preventDefault();
-    const href = event.currentTarget.getAttribute("href");
-    const section = document.querySelector(href);
-
-    // Ativar o scrool para que ele vai suave com esse novo methodo
-    section.scrollIntoView({
-      behavior: "smooth",
-      black: "start",
-    });
-  }
-
-  linksInternos.forEach((link) => {
-    link.addEventListener("click", scrollToSection);
-  });
-}
-initScrollsuave();
-
-// animação no scroll quando for rolando para baixa , fazendo animação suave
-function initAnimacaoScroll() {
-  const sections = document.querySelectorAll(".js-scroll");
-  if (sections.length) {
-    const windowMetade = window.innerHeight * 0.6;
-    function animaScroll() {
-      sections.forEach((section) => {
-        const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisibile = sectionTop - windowMetade < 0;
-        if (isSectionVisibile) {
-          section.classList.add("ativo");
-        }
-      });
-    }
-    animaScroll();
-    window.addEventListener("scroll", animaScroll);
-  }
-}
+initScrollSuave();
 initAnimacaoScroll();
+initAccordion();
+initTabNav();
+initModal();
