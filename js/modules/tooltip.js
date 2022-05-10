8c7ceae11dfc1a7f4fdd7be7f36d1c2409791bd1
Lucas Braga <lucasbraga_a@outlook.com>
2022-05-10 17:40:01 -0300

    evento tooltip criada

---
+++ js/modules/tooltip.js
@@ -0,0 +1,44 @@
+export default function initToolTip() {
+  const toolTipe = document.querySelectorAll("[data-tooltip]");
+
+  toolTipe.forEach((item) => {
+    item.addEventListener("mouseover", onMouseOver);
+  });
+
+  function onMouseOver(event) {
+    const toolTipBox = criarToolTipBox(this);
+    this.addEventListener("mousemove", onMouseMove);
+
+    onMouseMove.toolTipBox = toolTipBox;
+    onMouseLeave.toolTipBox = toolTipBox;
+    onMouseLeave.element = this;
+
+    this.addEventListener("mouseleave", onMouseLeave);
+  }
+  // objeto criado para remove a tooltip quando passar o mouse outra vez
+  const onMouseLeave = {
+    //metodo que faz com que o objeto faça referencia com a função em cima
+    handleEvent() {
+      this.toolTipBox.remove();
+      this.element.removeEventListener("mouseleave", onMouseLeave);
+      this.element.removeEventListener("mousemove", onMouseMove);
+    },
+  };
+
+  //objeto que faz com que o tooltip mova junto com o mouse
+  const onMouseMove = {
+    handleEvent(event) {
+      this.toolTipBox.style.top = event.pageY + 20 + "px";
+      this.toolTipBox.style.left = event.pageX + 20 + "px";
+    },
+  };
+
+  function criarToolTipBox(element) {
+    const toolTipBox = document.createElement("div");
+    const text = element.getAttribute("aria-label");
+    toolTipBox.classList.add("tooltip");
+    toolTipBox.innerText = text;
+    document.body.appendChild(toolTipBox);
+    return toolTipBox;
+  }
+}
