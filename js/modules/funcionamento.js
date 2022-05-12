4fda3df2e7d7e623af4fdfad446a53308cbaebce
Lucas Braga <lucasbraga_a@outlook.com>
2022-05-12 19:47:47 -0300

    interacao com horario

---
+++ js/modules/funcionamento.js
@@ -0,0 +1,19 @@
+export default function initFuncionamento() {
+  const funcionamento = document.querySelector("[data-semana]");
+  const diasSemana = funcionamento.dataset.semana.split(",").map(Number);
+  const horarioSEmana = funcionamento.dataset.horario.split(",").map(Number);
+
+  console.log(horarioSEmana);
+
+  const dataAgora = new Date();
+  const diaAgora = dataAgora.getDay();
+  const horarioAgora = dataAgora.getHours();
+
+  const semanaAberto = diasSemana.indexOf(diaAgora) !== -1;
+  const horarioAberto =
+    horarioAgora >= horarioSEmana[0] && horarioAgora < horarioSEmana[1];
+
+  if (semanaAberto && horarioAberto) {
+    funcionamento.classList.add("aberto");
+  }
+}
